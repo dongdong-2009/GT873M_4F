0a976df5fbf2fdb84677d09c60fc3c97d0e2b4fe
wangxy <wangxy@gw.com>
2014-02-25 16:42:47 +0800

    osSem api modified

--- src/onu/3rd/driver/marvell/osSem.c
+++ src/onu/3rd/driver/marvell/osSem.c
@@ -12,16 +12,63 @@
 *
 *******************************************************************************/
 
+#include <stdio.h>
+#include <string.h>
 
 #include <msApi.h>
 
 #include "cs_cmn.h"
 #include "osal_api_core.h"
+#include "gtSem.h"
 
 GT_SEM osSemCreate( GT_SEM_BEGIN_STATE state);
 GT_STATUS osSemDelete(GT_SEM smid);
 GT_STATUS osSemWait(  GT_SEM smid, GT_U32 timeOut);
 GT_STATUS osSemSignal(GT_SEM smid);
+
+typedef struct{
+	GT_BOOL set;
+	GT_SEM  smid;
+}gt_sem_e_t;
+
+static gt_sem_e_t gt_sem_es[CS_OSAL_MAX_COUNT_SEM];
+static GT_BOOL gt_sem_init = GT_FALSE;
+
+static GT_U32  getFreeNameBit()
+{
+	GT_U32 i;
+
+	for(i=0; i<CS_OSAL_MAX_COUNT_SEM; i++)
+	{
+		if(gt_sem_es[i].set == GT_FALSE)
+			return i;
+	}
+
+	return i;
+}
+
+static void  setNameBit(GT_U32 pos, GT_BOOL en, GT_SEM id)
+{
+	if(pos < CS_OSAL_MAX_COUNT_SEM)
+	{
+		gt_sem_es[pos].set = en;
+		gt_sem_es[pos].smid = id;
+	}
+}
+
+static void clrNameBit(GT_SEM id)
+{
+	GT_U32 i;
+
+	for(i=0; i<CS_OSAL_MAX_COUNT_SEM; i++)
+	{
+		if(gt_sem_es[i].smid == id)
+		{
+			gt_sem_es[i].set = GT_FALSE;
+			break;
+		}
+	}
+}
 
 /*******************************************************************************
 * osSemCreate
@@ -48,10 +95,34 @@ GT_STATUS osSemSignal(GT_SEM smid);
 GT_SEM osSemCreate(GT_SEM_BEGIN_STATE state)
 {
 	GT_SEM osSemId;
-	if(cs_semaphore_init((unsigned int*)&osSemId, "swSemId", state, 0) == CS_E_OK)
-		return osSemId;
-	else
-		return GT_FAIL;
+	GT_U32 namebit = 0;
+
+	if(gt_sem_init)
+	{
+		memset(gt_sem_es, 0, sizeof(gt_sem_e_t)*CS_OSAL_MAX_COUNT_SEM);
+		gt_sem_init = GT_TRUE;
+	}
+
+	if((namebit = getFreeNameBit()) != CS_OSAL_MAX_COUNT_SEM)
+	{
+		char name[CS_OSAL_MAX_API_NAME] = "";
+
+		sprintf(name, "swSemid%lu", namebit);
+
+		if(cs_semaphore_init((unsigned int*)&osSemId, name, state, 0) == CS_E_OK)
+		{
+			cs_printf("%s (%lu)\n", __func__, osSemId);
+			setNameBit(namebit, GT_TRUE, osSemId);
+			return osSemId;
+		}
+		else
+		{
+			cs_printf("%s failed!\n", __func__);
+			return GT_ERROR;
+		}
+	}
+
+	return GT_ERROR;
 }
 
 /*******************************************************************************
@@ -76,6 +147,7 @@ GT_SEM osSemCreate(GT_SEM_BEGIN_STATE state)
 *******************************************************************************/
 GT_STATUS osSemDelete(GT_SEM smid)
 {
+	clrNameBit(smid);
 	if(cs_semaphore_destroy(smid) != CS_E_OK)
 		return GT_ERROR;
 	return GT_OK;
@@ -105,7 +177,10 @@ GT_STATUS osSemDelete(GT_SEM smid)
 *******************************************************************************/
 GT_STATUS osSemWait(GT_SEM smid, GT_U32 timeOut)
 {
-	if(cs_semaphore_wait(smid, timeOut) != CS_E_OK)
+
+	cs_int32 cs_timeout = (timeOut == OS_WAIT_FOREVER)?CS_OSAL_WAIT_FOREVER:timeOut;
+
+	if(cs_semaphore_wait(smid, cs_timeout) != CS_E_OK)
 		return GT_ERROR;
 	return GT_OK;
 }
