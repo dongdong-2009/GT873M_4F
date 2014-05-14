f88593260147e190e98f67995caf917a84450a4e
zhangjj <zhangjj@gw.com>
2014-05-14 10:03:48 +0800

    [研发改动]修改编译脚本，添加make clean操作

--- sheel_script.sh
+++ sheel_script.sh
@@ -1,28 +1,47 @@
 #!/bin/sh 
 echo ""
-echo "1.GT812C"
-echo "2.GT811D"
-echo "3.GT811G"
-echo "4.GT873_M_4F4S"
-echo ""
+echo "1.make GT812C"
+echo "2.make clean GT812C"
+echo "3.make GT811D"
+echo "4.make clean GT811D"
+echo "5.make GT811G"
+echo "6.make clean GT811G"
+echo "7.make GT873_M_4F4S"
+echo -n "8.make clean GT873_M_4F4S\n\n"
 read -p "Select Product Type NO.:" ptype
 echo ""
 if [ $ptype -eq 1 ];then
-	echo "You select GT812C"
+	echo "You select make GT812C"
 	sleep 2
 	make PRODUCT=GT812C USING_SW_TYPE=MRV
 	elif [ $ptype -eq 2 ];then
-	echo "You select GT811D"
+	echo "You select make clean GT812C"
+	sleep 2
+	make clean PRODUCT=GT812C USING_SW_TYPE=MRV
+	elif [ $ptype -eq 3 ];then
+	echo "You select make GT811D"
 	sleep 2
 	make PRODUCT=GT811D USING_SW_TYPE=RTK
-		elif [ $ptype -eq 3 ];then
-		echo "You select GT811G"
-		sleep 2
-		make PRODUCT=GT811G USING_SW_TYPE=RTK
-			elif [ $ptype -eq 4 ];then
-			echo "You select GT873_M_4F4S"
-			sleep 2
-			make PRODUCT=GT873_M_4F4S USING_SW_TYPE=RTK
+	elif [ $ptype -eq 4 ];then
+	echo "You select make clean GT811D"
+	sleep 2
+	make clean PRODUCT=GT811D USING_SW_TYPE=RTK
+	elif [ $ptype -eq 5 ];then
+	echo "You select make GT811G"
+	sleep 2
+	make PRODUCT=GT811G USING_SW_TYPE=RTK
+	elif [ $ptype -eq 6 ];then
+	echo "You select make clean GT811G"
+	sleep 2
+	make clean PRODUCT=GT811G USING_SW_TYPE=RTK
+	elif [ $ptype -eq 7 ];then
+	echo "You select make GT873_M_4F4S"
+	sleep 2
+	make PRODUCT=GT873_M_4F4S USING_SW_TYPE=RTK
+	elif [ $ptype -eq 8 ];then
+	echo "You select make clean GT873_M_4F4S"
+	sleep 2
+	make clean PRODUCT=GT873_M_4F4S USING_SW_TYPE=RTK
 else
 echo "Input error!"
 fi
